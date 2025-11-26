1ab0a535b3435512e055f2c2b81221f901545f0b
Olli <mrmccamley@gmail.com>
2025-11-26 15:58:16 +0000

    scripts add for app & db

---
+++ App_Scripts/prov-app.sh
@@ -0,0 +1,84 @@
+
+
+#!/bin/bash
+ 
+# purpose: provision software + configuration to run Sparta Node JS test app
+# works on: AWS EC2 instance running Ubuntu 22.04 LTS
+# works on: fresh VM and if run multiple times
+ 
+echo Update sources list...
+sudo apt update
+echo Done!
+echo
+ 
+echo Upgrade...
+sudo DEBIAN_FRONTEND=noninteractive apt upgrade -y
+echo Done!
+echo
+ 
+# install nginx - will later be used as a reverse proxy
+echo Install nginx...
+sudo DEBIAN_FRONTEND=noninteractive apt install nginx -y
+echo Done!
+echo
+ 
+# configure reverse proxy here
+ 
+echo Apply changes to Nginx config...
+sudo systemctl restart nginx
+echo Done!
+echo
+ 
+# nginx by default already enabled other uncomment the next line
+# sudo systemctl enable nginx
+ 
+echo Setup for install of Node JS 20...
+curl -sL https://deb.nodesource.com/setup_20.x -o nodesource_setup.sh
+# TEST TO MAKE SURE NO USER INPUT
+sudo DEBIAN_FRONTEND=noninteractive bash nodesource_setup.sh
+echo Done!
+echo
+ 
+# NODE JS Install
+echo Install Node JS 20...
+# TEST TO MAKE SURE NO USER INPUT
+sudo DEBIAN_FRONTEND=noninteractive apt install nodejs -y
+echo Done!
+echo
+ 
+# copy app code to VM
+# git clone <endpoint to your GitHub public repo with app code> repo
+echo git clone repo
+ git clone https://github.com/OlliKm/tech515_sparta_app.git repo
+ echo
+
+
+# cd into the app folder
+cd repo/app
+ 
+#connect to database - may need to change ip for new load of system
+# original had issues of not working - due to me not having the :3000/posts in url on had /posts
+export DB_HOST=mongodb://172.31.53.160/posts
+printenv DB_HOST
+ 
+
+
+echo Run npm install...
+npm install
+echo Done!
+echo
+ 
+ #put in the reverse proxy 
+sudo sed -i "s|try_files \$uri \$uri/ =404;|proxy_pass http://127.0.0.1:3000;|" /etc/nginx/sites-available/default
+
+sudo systemctl reload nginx
+
+
+ sudo npm install -g pm2
+ pm2 startup systemd
+ sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u ubuntu --hp /home/ubuntu
+pm2 save
+
+cd repo/app
+pm2 start app.js &
+#pm2 stop app.js
---
+++ App_Scripts/prov-db.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+ 
+# purpose: provision Mongo DB v7 to work with Sparta Node JS test app
+# works on: AWS EC2 instance running Ubuntu 22.04 LTS
+# works on: fresh VM and if run multiple times
+ 
+echo Update sources list...
+sudo apt update
+echo Done!
+echo
+ 
+echo Upgrade...
+sudo DEBIAN_FRONTEND=noninteractive apt upgrade -y
+echo Done!
+echo
+
+
+echo install GPG for Mongo DB
+curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | \
+   sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg \
+   --dearmor
+echo Done!
+echo 
+
+echo create list file for mongo db...
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+echo Done!
+echo
+
+echo run update...
+sudo apt-get update
+echo done!
+echo
+
+echo Install Mongo DB..
+#need to tet so it doesnt ask for ui input of fresh vm
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y \
+   mongodb-org=7.0.24 \
+   mongodb-org-database=7.0.24 \
+   mongodb-org-server=7.0.24 \
+   mongodb-mongosh \
+   mongodb-org-shell=7.0.24 \
+   mongodb-org-mongos=7.0.24 \
+   mongodb-org-tools=7.0.24 \
+   mongodb-org-database-tools-extra=7.0.24
+   echo Done!
+   echo 
+
+   # configure bind IP # to 0.0.0.0 - with sed command
+echo "Configuring mongod.conf bindIp..."
+sudo sed -i 's/^  bindIp: .*/  bindIp: 0.0.0.0/' /etc/mongod.conf
+echo "Done!"
+echo
+   
+   #start mongo db
+echo Start MongoDB...
+sudo systemctl start mongod
+echo Done!
+echo 
+   #enable mongo db
+echo Enable Mongo DB...
+sudo systemctl enable mongod
+echo Done!
+echo 
